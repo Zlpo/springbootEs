9f1567b0fc51eb5923e9298bd7edf3368b7abd95
zhurongzhou <923998529@qq.com>
2019-06-25 14:19:30 +0800

    first commit

---
+++ src/main/java/com/zz/mees/MeEsApplication.java
@@ -0,0 +1,13 @@
+package com.zz.mees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeEsApplication.class, args);
+    }
+
+}
